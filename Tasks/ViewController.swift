fa9af391edb018eae39f81d62be7ad2f1d6cfcac
Steven Berard <steven@heycoastal.com>
2020-02-11 18:35:55 -0800

    Initial Commit

---
+++ Tasks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tasks
+//
+//  Created by Steven Berard on 2/11/20.
+//  Copyright © 2020 Steven Berard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
